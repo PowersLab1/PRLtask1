f44dac2d25708a213570be953a76f3760e644da5
PowersLab1 <powerslab3@gmail.com>
2023-10-17 13:58:31 -0400

    Update script.js
    Added in winning money as only 2 outcomes and changed the display text to have paragraphs between them

--- public/script.js
+++ public/script.js
@@ -1,10 +1,3 @@
-//Probabilistic Reversal Learning Task in JavaScript from ChatGPT4
-//-----------------------------
-//Please write a script in javascript that produces the following online probablistic learning task for participants to play on their computer.
-//"this task requires trial-wise binary decisions between two fractal stimuli. On each trial, the same pair of fractal stimuli are presented (one on the left, one on the right corresponding to image 1 and image 2), each paired with a probability of giving a participant 10 points vs. costing the participant 10 points.  One image rewards participants 10 points 80% of the time while costing them 10 points 20% of the time; the other fractal rewards participants 10 points 20% of the time and costs them 10 points 80% of the time. The task starts by presenting the images on the screen. Then participants have a maximum of 4 s to make a decision (by pressing the “1” key to indicate the left image and the “2” key to indicate the right image), followed by a 5-second delay displaying the choice and a 2-second presentation of the decision outcome (correct or incorrect) with reward feedback (they either lose 10 points or win 10 points). If no response is made during the decision period, a time-out occurs and a screen saying "too slow!" is displayed for 8 s, instead of the delay and outcome screens. The across-trial task structure incorporates ‘switches’, or reversals, in a block-wise fashion in terms of which of the cues is most likely (80% probability) or least likely (20%) to be the correct, rewarded choice on that trial. The first 55 trials feature one stable set of reward contingencies between the two fractal options. Then, which fractal is more likely to lead to a reward is switched for the next 45 trials. Then it switches back for 20 trials. Then it switches again for 20 trials, and again one last time for 20 trials.  The task consists of 160 trials in total.”
-//The script must produce an array at the end that will upload to a server. The array should contain a row for each trial and columns containing values of 1) which image the participant picked (1 or 2 for left versus right and -999 if they didn’t select anything), 2) whether their choice was rewarded or not (0 indicating they lost 10 points and 1 indicating they were rewarded 10 points), 3) the running total number of points the participant has, 4) the reward probability of fractal image 1 for that trial, 5) the reward probability of fractal image 2 for that trial, and 6) the time it took between presentation of the 2 fractal images and a decision being made (the participant hitting key 1 or 2). For the fractal images themselves please use https://github.com/Powers-Lab/Tasks/blob/4a88089bd4b32171714b643751bcdc3a2cfe186d/Psychedelic/PRL/html/resources/Fractal1.png and  https://github.com/Powers-Lab/Tasks/blob/4a88089bd4b32171714b643751bcdc3a2cfe186d/Psychedelic/PRL/html/resources/Fractal10.png for fractal 1 and fractal 2
-
-
 <!DOCTYPE html>
 <html lang="en">
 
@@ -103,26 +96,24 @@
 <body>
 
 <div id="start-screen">
-    Please enter your unique Participant Record ID and then press enter
+    Please enter your unique Participant Record ID and then press ENTER
     <input type="text" id="participant-id">
 </div>
 
 <div id="instruction-screen" style="display: none;">
-    Welcome to the game! 
+   <p>Welcome to the game!</p>
     
-    You will be repeatedly presented with an abstract images on the left and one on teh right. You will be asked to pick one.
-    Picking an image may win you points, or it may cost you points.
+    <p>You will be presented with an image on the left and one on the right. You will be asked to pick one. Picking an image wins you money, but one image gives you more money than the other.</p>
 
-    One image is more likely than the other to give you points than cost you points; you will need to figure out which!
-    NOTE: the image which gives you more points may switch as the game continues!
+    <p>NOTE: the image which gives you more money may SWITCH as the game continues!</p>
 
-    Your goal is to win as many points as possible. 
+    <p>Your goal is to win as much money as possible.</p> 
 
-    When you are ready to begin, please press "1" on your keyboard to start the game
+    <p>When you are ready to begin, please press "1" on your keyboard to start the game</p>
 </div>
 
 <div id="game-container" style="display: none;">
-    <div id="instruction">Pick the left or right image to win points!</div>
+    <div id="instruction">Pick the left or right image to win money!</div>
     <div id="fractals">
         <div class="indicator" id="left-indicator"></div>
         <img src="https://raw.githubusercontent.com/maxsupergreenwald/PRLResources/main/resources/Fractal1.png" class="fractal" id="fractal1">
@@ -130,7 +121,7 @@
         <div class="indicator" id="right-indicator"></div>
     </div>
     <div id="feedback"></div>
-    <div id="points">Points: 0</div>
+    <div id="points">Winnings: 0</div>
 </div>
 
 <div id="fixation-cross">+</div>
@@ -206,7 +197,7 @@
         },
         showFeedback(choice, outcome, decisionTime) {
             const {fractal1, fractal2} = this.rewardProbs[this.currentProbIndex];
-            const trialData = {
+            const trialData = { //This block of code shows how data is handled for each trial; all the below variables are added to the object trialData for each individual trial
                 participantId,
                 choice,
                 outcome,
@@ -215,25 +206,25 @@
                 rewardProbFractal2: fractal2,
                 decisionTime
             };
-            this.trials.push(trialData);
+            this.trials.push(trialData); //the trials property of the game object is an array that stores the data for each trial as the user progresses through the task. The game.trials array is initialized as an empty array. After each trial, an object containing the trial data is pushed to the game.trials array.
 
             document.getElementById('fractals').style.display = 'none';
             document.getElementById('instruction').style.display = 'none';
             document.getElementById('feedback').style.display = 'block';
-            document.getElementById('points').innerText = `Points: ${this.totalPoints}`;
+            document.getElementById('points').innerText = `Winnings: ${this.totalPoints}`;
             document.getElementById('points').style.display = 'block';
             
             const feedback = document.getElementById("feedback");
             if (choice === -999) {
-                feedback.innerText = "Too slow! You get 0 points";
+                feedback.innerText = "Too slow! You get $0";
             } else if (outcome) {
                 feedback.classList.add('positive-feedback');
-                feedback.innerText = "You win 10 points!";
-                this.totalPoints += 10;
+                feedback.innerText = "You get $0.75";
+                this.totalPoints += 0.75;
             } else {
                 feedback.classList.add('negative-feedback');
-                feedback.innerText = "You lose 10 points!";
-                this.totalPoints -= 10;
+                feedback.innerText = "You get $0.25";
+                this.totalPoints -= 0.25;
             }
 
             setTimeout(() => {
@@ -249,13 +240,13 @@
         },
         startNextTrial() {
             if (this.trials.length === this.trialLimits.reduce((a, b) => a + b, 0)) {
-                const json = JSON.stringify(this.trials, null, 2);
-                const blob = new Blob([json], {type: "application/json"});
-                const url = URL.createObjectURL(blob);
+                const json = JSON.stringify(this.trials, null, 2); //this converts array game.trials into a JSON string
+                const blob = new Blob([json], {type: "application/json"}); 
+                const url = URL.createObjectURL(blob); 
                 const downloadLink = document.getElementById('downloadLink');
                 downloadLink.href = url;
                 downloadLink.download = 'results.json';
-                downloadLink.style.display = 'block';
+                downloadLink.style.display = 'block'; //the above lines take the JSON string and make  it accessible by a download link. 
                 document.getElementById('completion-message').style.display = 'block';
                 document.getElementById('game-container').style.display = 'none';
             } else if (this.trials.length === this.trialLimits.slice(0, this.currentProbIndex + 1).reduce((a, b) => a + b, 0)) {
