e314390b760213ac3c5d5f67be9c131f2687981d
Max Greenwald <maximillian.greenwald@yale.edu>
2023-10-21 14:32:28 -0400

    Added a log.console() command to the games.trial object to match the ThinkNoThinkV4 script to maximize teh chances that it works well with the lab.jswrapper script!!! Now it still does the whole window.parent thing too so data gets passed on in two ways...  hopefully that's ok

--- src/index.js
+++ src/index.js
@@ -12,3 +12,17 @@ ReactDOM.render(
     </Router>,
     document.getElementById("root")
 );
+
+////USE THIS IF CONCERNED ABOUT CONSOLE BASENAME AND HASHROUTER WARNINGS
+////The script below can replace the script above and have the task safely run w/o any error messages, but it's unclear what the value of this would be. 
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import App from './App';
+// import { HashRouter as Router } from "react-router-dom";
+
+// ReactDOM.render(
+//     <Router>
+//         <App />
+//     </Router>,
+//     document.getElementById('root')
+// );
